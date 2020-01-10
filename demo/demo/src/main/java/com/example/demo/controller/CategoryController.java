6a5649ea521d63b97ef22a874a89536a5487c2f0
DonLalo222 <g.vasquez.eduardo@gmail.com>
2020-01-10 13:09:44 -0300

    Signed-off-by: DonLalo222 <g.vasquez.eduardo@gmail.com>

--- demo/demo/src/main/java/com/example/demo/controller/CategoryController.java
+++ demo/demo/src/main/java/com/example/demo/controller/CategoryController.java
@@ -3,13 +3,24 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.model.Category;
 
 @Controller
 public class CategoryController {
-	@GetMapping("/categoria/indice")
+	@GetMapping("/categoria/index")
 	public String index(Model model) {
-		model.addAttribute("title", "categoria - index");
+		model.addAttribute("title", "Categoria - Index");
 		return "category/index";
+	}
+	
+	@GetMapping("/categoria/crear")
+	public String index(Model model, @ModelAttribute Category category) {
+		model.addAttribute("title", "Categoria - Crear");
+		model.addAttribute("category", category);
+		return "category/create";
 	}
 
 }
